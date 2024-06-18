7a8b0049634f3f0ef9857f44050162726af00f35
Victor Dupla TI <imp.mannelli@gmail.com>
2024-06-18 17:24:35 -0300

    fix: not getting cache messages when entering after message was sent

---
+++ prisma/migrations/20240618155521_fix_room_id_from_message_as_unique/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "messages_room_id_key";
--- src/modules/messages/messages.gateway.ts
+++ src/modules/messages/messages.gateway.ts
@@ -5,7 +5,7 @@ import { MessageSchema } from './messages.schema';
 import { MessageType } from './messages.type';
 import { Server, Socket } from 'socket.io';
 import Bottleneck from 'bottleneck';
-
+import { z } from 'zod';
 @Injectable()
 @WebSocketGateway(8080, { namespace: '/messages', cors: true })
 export class MessagesGateway {
@@ -36,7 +36,17 @@ export class MessagesGateway {
 
     if (this.messagesCache.length > 0) {
       try {
-        await this.limiter.schedule(() => this.messagesService.postMessages({ messages: this.messagesCache }));
+        const ParsedMessages = this.messagesCache.map((message) => {
+          return {
+            message_id: message.message_id,
+            message: message.message,
+            room_id: message.room_id,
+            author_id: message.author_id,
+            receiver_id: message.receiver_id,
+            created_at: message.created_at,
+          };
+        });
+        await this.limiter.schedule(() => this.messagesService.postMessages({ messages: ParsedMessages }));
         this.messagesCache = [];
         if (this.timeoutHandle) {
           clearTimeout(this.timeoutHandle);
@@ -56,15 +66,33 @@ export class MessagesGateway {
   server: Server;
 
   @SubscribeMessage('joinRoom')
-  handleJoinRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
-    client.join(room);
-    client.emit('joinedRoom', room);
+  async handleJoinRoom(@MessageBody() JoinRoomBody: { room: string; user_id: string }, @ConnectedSocket() client: Socket): Promise<void> {
+    z.object({ room: z.string(), user_id: z.string() }).parse(JoinRoomBody);
+    client.join(JoinRoomBody.room);
+    // console.log('user joined');
+    // client.emit('joinedRoom', room);
+
+    const cachedMessages = this.messagesCache.reduce((acc, msg) => {
+      if (msg.room_id === JoinRoomBody.room) {
+        acc.push({
+          message_id: msg.message_id,
+          message: msg.message,
+          author: msg.author,
+          created_at: msg.created_at,
+        });
+      }
+      return acc;
+    }, []);
+    const historicalMessages = await this.messagesService.getMessages({ user_id: JoinRoomBody.user_id, room_id: JoinRoomBody.room });
+    client.emit('joinedRoom', { storedMessages: [...historicalMessages, ...cachedMessages] });
   }
 
   @SubscribeMessage('leaveRoom')
   handleLeaveRoom(@MessageBody() room: string, @ConnectedSocket() client: Socket): void {
+    if (this.messagesCache.length > 0) this.sendMessagesToDB();
     client.leave(room);
-    client.emit('leftRoom', room);
+    // console.log('user left');
+    // client.emit('leftRoom', room);
   }
 
   @SubscribeMessage('message')
@@ -75,14 +103,14 @@ export class MessagesGateway {
       throw new UnprocessableEntityException();
     }
     this.server.to(messageBody.room).emit('message', { message: messageBody.message, sender: client.id });
-
     this.messagesCache.push({
       message_id: messageBody.message.message_id,
       message: messageBody.message.message,
+      room_id: messageBody.room,
       author_id: messageBody.message.author_id,
+      author: messageBody.message.author,
       receiver_id: messageBody.message.receiver_id,
       created_at: messageBody.message.created_at,
-      room_id: messageBody.room,
     });
 
     if (this.messagesCache.length === 1) {
